2b0a55ed32f7f1afe6d177d34ce0940e4defc0a0
pavithram08 <124552975+pavithram08@users.noreply.github.com>
2023-04-13 10:54:16 +0530

    Add files via upload

---
+++ Flask/static/js/main.js
@@ -0,0 +1,88 @@
+let time = new Date();
+let hour = time.getHours();
+let minute = time.getMinutes();
+const url = "https://sodhanai.onrender.com";
+$(".set-time").text(`${hour}:${minute}`);
+
+function getPredictions(text) {
+    $.ajax({
+        url: url + "/predict",
+        method: "POST",
+        data: {
+            text: text,
+        },
+        crossDomain: true,
+        success: function(res) {
+            console.log(res);
+            let time = new Date();
+            let hour = time.getHours();
+            let minute = time.getMinutes();
+            var ml_pred = res.ml_pred;
+            var dl_pred = res.dl_pred;
+            let outputMsg = "";
+
+            console.log(ml_pred)
+            console.log(dl_pred)
+
+            if (ml_pred == "1" && dl_pred == "1") {
+                outputMsg = "Message is a Terrorism ideation";
+            } else if (ml_pred == "0" && dl_pred == "0") {
+                outputMsg = "Message is not realted to terrorism ideation";
+            } else if (
+                (ml_pred == "0" && dl_pred == "1") ||
+                (ml_pred == "1" && dl_pred == "0")
+            ) {
+                outputMsg = "Message may be or may not be related terrorism ideation";
+            } else {
+                outputMsg = "Sorry,can't figure out!";
+            }
+
+            $("#new-res").append(`
+      <div class="msg right-msg">
+      <div class="msg-img"
+      style="background-image: url(https://cdn-icons-png.flaticon.com/512/163/163847.png)">
+      </div>
+      <div class="msg-bubble">
+          <div class="msg-info">
+              <div class="msg-info-name">Champ</div>
+              <div class="msg-info-time set-time">${hour}:${minute}</div>
+          </div>
+    
+          <div class="msg-text">
+              ${text}
+          </div>
+      </div>
+    </div>
+    
+    <div class="msg left-msg">
+    <div class="msg-img"
+    style="background-image: url(https://cdn-icons-png.flaticon.com/512/163/163847.png)">
+    
+    </div>
+    
+    <div class="msg-bubble">
+        <div class="msg-info">
+            <div class="msg-info-name">BOT</div>
+            <div class="msg-info-time set-time">${hour}:${minute}</div>
+        </div>
+    
+        <div class="msg-text">
+           ${outputMsg}
+        </div>
+    </div>
+    </div>
+    `);
+        },
+        error: function(err) {
+            console.log(err);
+        },
+    });
+    return;
+}
+
+function submitHandler() {
+    var input = $("#user-input").val();
+    if (input == "") return;
+    getPredictions(input);
+    $("#user-input").val("");
+}
